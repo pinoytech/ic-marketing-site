4773278cf193deb62adae7da4f9a63376c526c0d
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-18 07:45:20 +0800

    Attempt

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+            build: {
+                manifest: true,
+                outDir: "public/build",
+            },
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
