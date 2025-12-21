3b8d247597a13a3a3ea570a58a3acb03a87835dd
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-21 13:15:31 +0800

    WIP

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -17,13 +17,13 @@ class EventFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->sentence,
-            'event_webinar_date' => $this->faker->date,
+            'title' => $this->faker->sentence(),
+            'event_webinar_date' => $this->faker->date(),
             'image' => 'http://dummyimage.com/315x197.png',
             'status' => $this->faker->randomElement(['upcoming', 'past']),
-            'register_link' => $this->faker->url,
-            'watch_link' => $this->faker->url,
-            'description' => $this->faker->paragraph,
+            'register_link' => $this->faker->url(),
+            'watch_link' => $this->faker->url(),
+            'description' => $this->faker->paragraph(),
         ];
     }
 }
