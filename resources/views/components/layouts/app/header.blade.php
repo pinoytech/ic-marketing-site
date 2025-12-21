5952a3b2069c4fa7d434ee350da76321c7cbe6a1
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-21 19:40:41 +0800

    Updated Dashboard text and links

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -107,12 +107,12 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                    {{ __('Repository') }}
+                <flux:navlist.item icon="folder-git-2" href="https://app.infinitcare.co" target="_blank">
+                    {{ __('App') }}
                 </flux:navlist.item>
 
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                    {{ __('Documentation') }}
+                <flux:navlist.item icon="book-open-text" href="https://infinitcare.co" target="_blank">
+                    {{ __('Website') }}
                 </flux:navlist.item>
             </flux:navlist>
         </flux:sidebar>
