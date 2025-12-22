de92d03471620b0f1fab6d2d5856e000840915b3
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-23 00:00:11 +0800

    Favicon update

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 {{-- <link rel="icon" href="/favicon.png" sizes="any"> --}}
-{{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+<link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="icon" href="/favicon.png" type="image/png">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
