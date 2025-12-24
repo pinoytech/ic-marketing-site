c1c3fb6fb2a33157da61c94fd21893dfd3c9443a
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-24 08:09:17 +0800

    Added media link to the resources page

---
+++ resources/views/resources/media.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+  <div class="flex flex-col gap-8 my-auto justify-center items-center w-full">
+    <div class="w-full max-w-screen-xl mx-auto pt-8 px-4 md:px-0 md:pt-12 flex flex-col">
+      <div class="flex flex-col gap-8 text-left">
+        <a href='{{ route('resources.index') }}' class='text-teal-500 text-sm'>&larr; Back to Resources</a>
+        <h2 class="text-4xl font-semibold text-left mb-8">Media</h2>
+      </div>
+    </div>
+  </div>
+  @livewire('media-full')
+  <x-semi-footer-transform />
+</x-layout>
