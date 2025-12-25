41d771a1bfd5c04b8ac8fa030640f29c0fd9fde4
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-25 09:32:53 +0800

    Updated color of the navigation hamburger

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,7 +28,7 @@
             <img src="/images/logo.png" height="39px" width="129px" alt="InfinitCare Logo" class="cursor-pointer w-auto"/>
           </a>
 
-          <button id="mobile-menu-button" aria-controls="mobile-menu" aria-expanded="false" class="md:hidden p-2 mr-3 rounded-md text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-teal-500">
+          <button id="mobile-menu-button" aria-controls="mobile-menu" aria-expanded="false" class="md:hidden p-2 mr-3 rounded-md text-teal-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-teal-500">
             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
