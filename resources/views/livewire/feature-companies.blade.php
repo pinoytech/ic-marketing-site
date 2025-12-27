021d9d2e2395cb54bacf2bf5790d22423dcb80ff
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-27 09:28:57 +0800

    Feature companies enhancement

--- resources/views/livewire/feature-companies.blade.php
+++ resources/views/livewire/feature-companies.blade.php
@@ -3,29 +3,47 @@
     <div class="flex flex-row gap-4 justify-center items-center w-full py-4">
       <h2 class="text-3xl font-semibold text-center">Trusted by over 100 companies of all sizes</h2>
     </div>
-    <div class="w-3/4 flex flex-row flex-wrap justify-center items-center">
+    <div class="w-3/4 flex flex-row justify-center items-center">
       @foreach ($featuredCompanies as $company)
         <div>
           @if($company->logo_path)
-            <img src="{{ Storage::disk('resource')->url($company->logo_path) }}" alt="{{ $company->name }}" class="h-24 md:h-24 mx-4 my-8 inline-block object-contain" loading="lazy" />
+            <img src="{{ Storage::disk('resource')->url($company->logo_path) }}" alt="{{ $company->name }}" class="h-12 md:h-24 mx-4 my-8 inline-block object-contain" loading="lazy" />
           @else
-            <img src="{{ $company->logo_url }}" alt="{{ $company->name }}" class="h-24 md:h-24 mx-4 my-8 inline-block object-contain" loading="lazy" />
+            <img src="{{ $company->logo_url }}" alt="{{ $company->name }}" class="h-8 md:h-24 mx-4 my-8 inline-block object-contain" loading="lazy" />
           @endif
         </div>
       @endforeach
     </div>
   </div>
   <div class="w-full">
-    <div class='w-3/4 flex flex-row flex-wrap justify-center items-center mx-auto'>
-      @foreach ($nonFeaturedCompanies as $company)
-        <div>
-          @if($company->logo_path)
-            <img src="{{ Storage::disk('resource')->url($company->logo_path) }}" alt="{{ $company->name }}" class="h-18 md:h-18 mx-4 my-8 inline-block object-contain" loading="lazy" />
-          @else
-            <img src="{{ $company->logo_url }}" alt="{{ $company->name }}" class="h-18 md:h-18 mx-4 my-8 inline-block object-contain" loading="lazy" />
-          @endif
-        </div>
-      @endforeach
+  <div class="w-3/4 overflow-hidden relative mx-auto before:absolute before:left-0 before:top-0 before:bottom-0 before:w-32 before:bg-gradient-to-r before:from-white before:to-transparent before:z-10 after:absolute after:right-0 after:top-0 after:bottom-0 after:w-32 after:bg-gradient-to-l after:from-white after:to-transparent after:z-10">
+    <div class="flex" style="animation: marquee 20s linear infinite; width: max-content;">
+      <div class="flex">
+        @foreach ($nonFeaturedCompanies as $company)
+          <div class="flex-shrink-0">
+            @if($company->logo_path)
+              <img src="{{ Storage::disk('resource')->url($company->logo_path) }}" alt="{{ $company->name }}" class="h-8 md:h-18 mx-4 my-8 object-contain grayscale hover:grayscale-0 transition-all" loading="lazy" />
+            @else
+              <img src="{{ $company->logo_url }}" alt="{{ $company->name }}" class="h-6 md:h-18 mx-4 my-8 object-contain" loading="lazy" />
+            @endif
+          </div>
+        @endforeach
+      </div>
+      {{-- Exact duplicate --}}
+      <div class="flex">
+        @foreach ($nonFeaturedCompanies as $company)
+          <div class="flex-shrink-0">
+            @if($company->logo_path)
+              <img src="{{ Storage::disk('resource')->url($company->logo_path) }}" alt="{{ $company->name }}" class="h-8 md:h-18 mx-4 my-8 object-contain grayscale hover:grayscale-0 transition-all" loading="lazy" />
+            @else
+              <img src="{{ $company->logo_url }}" alt="{{ $company->name }}" class="h-6 md:h-18 mx-4 my-8 object-contain" loading="lazy" />
+            @endif
+          </div>
+        @endforeach
+      </div>
     </div>
   </div>
 </div>
+</div>
+
+
