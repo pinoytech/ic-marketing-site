f9b1a6fa8cce42a152bd794c7302ca86d892a620
PinoyTech <64101+pinoytech@users.noreply.github.com>
2025-12-28 09:32:03 +0800

    Added link from semi footer to booking page

--- resources/views/components/semi-footer-transform.blade.php
+++ resources/views/components/semi-footer-transform.blade.php
@@ -3,7 +3,7 @@
     <h3 class="text-4xl text-white text-center pt-8">Ready to transform your workplace culture?</h3>
     <p class="text-white font-semibold text-center">Our sales team will reach out to schedule a demo with you or see how we can help.</p>
     <div>
-      <button class="h-12 cursor-pointer font-semibold text-sm w-64 bg-white border-transparent rounded-lg px-4 py-2 text-teal-500">Book a Discovery Call</button>
+      <a href={{route('booking')}}><button class="h-12 cursor-pointer font-semibold text-sm w-64 bg-white border-transparent rounded-lg px-4 py-2 text-teal-500">Book a Discovery Call</button></a>
     </div>
     <p class="text-white my-8 text-center">Looking for help as a member or employee? Email us at <a href="mailto:info@infinitcare.co" class="text-teal-200">info@infinitcare.co</span></a>
   </div>
