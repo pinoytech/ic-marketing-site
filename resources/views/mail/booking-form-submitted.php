9204b0b27b8133ef3ca2aa64710415a87ef5d03c
PinoyTech <64101+pinoytech@users.noreply.github.com>
2026-01-25 17:25:56 +0800

    Sending email with data from form

--- resources/views/mail/booking-form-submitted.php
+++ resources/views/mail/booking-form-submitted.php
@@ -1,3 +1,14 @@
-<p>
-  Test email from InfinitCare Marketing Site Booking Form
-</p>
+<h4>
+  Email from InfinitCare Marketing Site Booking Form
+</h4>
+
+<ul>
+  <li><strong>Full Name:</strong> <?= $fullName ?></li>
+  <li><strong>Mobile Number:</strong> <?= $mobileNumber ?></li>
+  <li><strong>Email Address:</strong> <?= $emailAddress ?></li>
+  <li><strong>Company Name:</strong> <?= $companyName ?></li>
+  <li><strong>Position:</strong> <?= $position ?></li>
+  <li><strong>Number of Employees:</strong> <?= $numberOfEmployees ?></li>
+  <li><strong>Referral Source:</strong> <?= $referralSource ?></li>
+  <li><strong>Additional Message:</strong> <?= $additionalMessage ?></li>
+</ul>
